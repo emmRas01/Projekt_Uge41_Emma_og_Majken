c4d6392c266a1ac1f22ae4b635c2fa3bb1ffb832
Majken <majros01@easv365.dk>
2025-10-03 09:43:48 +0200

    skabelon

--- src/main/java/com/example/projekt_uge41_majken_og_emma/Brik.java
+++ src/main/java/com/example/projekt_uge41_majken_og_emma/Brik.java
@@ -1,6 +1,28 @@
 package com.example.projekt_uge41_majken_og_emma;
 
-public class Brik
-{
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
+
+public class Brik extends ImageView {
+
+    private Image forside, bagside;
+
+    public Brik(int x, int y, String filnavn) {
+        // Sæt ny brik op med forside- og bagsidebillede (alle brikker har samme bagside).
+        // Vis forsiden som udgangspunkt
+        forside = new Image(getClass().getResource(filnavn).toString());
+        bagside = new Image(getClass().getResource("bagside.png").toString());
+        setImage(forside);
+        // Placér brikken: lav selv koordinaterne, så brikkerne spredes
+        setX(x*80);
+        setY(y*80);
+    }
+
+    public void vend() {
+        // Her skal brikken vendes.
+        // Lige nu udskrives lidt oplysninger om brikken og bagsiden vises
+        System.out.println("brik " + getX() + "," + getY());
+        setImage(bagside);
+    }
 }
--- src/main/java/com/example/projekt_uge41_majken_og_emma/VendeSpil.java
+++ src/main/java/com/example/projekt_uge41_majken_og_emma/VendeSpil.java
@@ -1,6 +1,52 @@
 package com.example.projekt_uge41_majken_og_emma;
 
-public class VendeSpil
-{
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
+import java.io.IOException;
+
+public class VendeSpil extends Application {
+
+    // Alle brikker holdes i arrayet brikker
+    private Brik[][] brikker;
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        Pane scenegraf = new Pane();
+
+        // eventHandler som senere sættes på hver brik, så man kan klikke med musun.
+        EventHandler<MouseEvent> eventHandler = new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent e) {
+                Brik b = (Brik)e.getSource();
+                b.vend();
+            }
+        };
+
+        // Her sættes banen op. Filen brik1.png skal findes i resource
+        brikker = new Brik[5][5];
+        for (int i=0; i<5; i++)
+            for (int j=0; j<5; j++) {
+                // Lav hver brik som et objekt "Brik" med billede
+                brikker[i][j] = new Brik(i, j, "brik1.png");
+                // Tilføj den til scenegrafen
+                scenegraf.getChildren().add(brikker[i][j]);
+                // Tilføj eventen til brikken
+                brikker[i][j].addEventFilter(MouseEvent.MOUSE_CLICKED, eventHandler);
+            }
+
+        // Sæt scenen op
+        Scene scene = new Scene(scenegraf, 600, 600);
+        stage.setTitle("MemorySkelet!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
 }
